1563236b3ff81ad8e843b7e140662cc55d69ff6d
Robi Abda'u <81896427+robiabdau@users.noreply.github.com>
2021-07-03 15:23:35 +0700

    Add files via upload

---
+++ components/Controller.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Controller is the customized base controller class.
+ * All controller classes for this application should extend from this base class.
+ */
+class Controller extends CController
+{
+	/**
+	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
+	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
+	 */
+	public $layout='//layouts/column1';
+	/**
+	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
+	 */
+	public $menu=array();
+	/**
+	 * @var array the breadcrumbs of the current page. The value of this property will
+	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
+	 * for more details on how to specify this property.
+	 */
+	public $breadcrumbs=array();
+    
+    public function pr($array) {
+        print "<pre>" . print_r($array, true) . "</pre>";
+    }
+
+    /**
+	 * @param string the string variable that will convert into CamelCase.
+	 * @return camelcased string.
+	 */
+    public function toCamelCase($string) {
+        return preg_replace('/(?:^|_)(.?)/e',"strtoupper('$1')",$string);
+    }
+    
+}
